e8dd2503c39feaf4fa56319edc2267ffdb1692d5
sametatabasch <sametatabasch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-28 21:20:40 +0000

    version 1.4.2
    git-svn-id: https://plugins.svn.wordpress.org/notices-duyurular/trunk@812081 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- default.js.php
+++ default.js.php
@@ -96,9 +96,9 @@ jQuery.fn.Window = function (content, isClass) {
 				'<h4></h4>' +
 				'<p><?php _e( 'If you do not want to see again this notice,click "do not show again".', $GB_Duyurular->textDomainString ) ?></p>' +
 				'<div id="yes-no" class="center">' +
-				'<button id="yes" class="btn"><?php _e( 'Do not show again', $GB_Duyurular->textDomainString ) ?></button> - <button id="no" class="btn"><?php _e( 'Close', $GB_Duyurular->textDomainString ) ?></button>' +
+					'<button id="yes" class="btn"><?php _e( 'Do not show again', $GB_Duyurular->textDomainString ) ?></button> - <button id="no" class="btn"><?php _e( 'Close', $GB_Duyurular->textDomainString ) ?></button>' +
 				'</div>' +
-				'</div>';
+			'</div>';
 var genislik = jQuery('#windowBox').width();
 		jQuery('#windowBox').find('.window').replaceWith(icerik);
 		jQuery('#windowBox .window').width(genislik);
@@ -111,9 +111,13 @@ var genislik = jQuery('#windowBox').width();
 				data: "GB_D_noticeId=" + currentId
 			});
 			this.content.splice(this.currentIndex, 1);
-			this.next();
-			if (this.content.length == 1)jQuery('.window-nav').remove();
-		}, this));
+			if (this.content.length > 0) {
+				this.next();
+				if (this.content.length == 1)jQuery('.window-nav').remove();
+			} else {
+				close(jQuery('#windowBackground'));
+			}
+}, this));
 		jQuery('#yes-no #no').click(jQuery.proxy(function () {
 			this.content.splice(this.currentIndex, 1);
 			if (this.content.length > 0) {
@@ -149,9 +153,9 @@ jQuery(document).ready(function () {
 		var reg = /\d/g;
 		currentId = currentId.match(reg).join('');
 var icerik ='<div class="bar alert alert-info">' +
-	'<h4></h4>' +
-	'<p><?php _e( 'If you do not want to see again this notice,click "do not show again".', $GB_Duyurular->textDomainString ) ?></p>' +
-	'<button id="yes" class="btn"><?php _e( 'Do not show again', $GB_Duyurular->textDomainString ) ?></button> - <button id="no" class="btn"><?php _e( 'Close', $GB_Duyurular->textDomainString ) ?></button>' +
+		'<h4></h4>' +
+		'<p><?php _e( 'If you do not want to see again this notice,click "do not show again".', $GB_Duyurular->textDomainString ) ?></p>' +
+		'<button id="yes" class="btn"><?php _e( 'Do not show again', $GB_Duyurular->textDomainString ) ?></button> - <button id="no" class="btn"><?php _e( 'Close', $GB_Duyurular->textDomainString ) ?></button>' +
 	'</div>';
 jQuery('.noticeContainer').find('.bar').replaceWith(icerik);
 		jQuery('#yes').click(function () {
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: sametatabasch
 Tags: notice, duyuru,post_type,cookie
 Requires at least: 3.0.1
 Tested up to: 3.7.1
-Stable tag: 1.4.1
+Stable tag: 1.4.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -44,6 +44,10 @@ Extract the zip file and just drop the contents in the wp-content/plugins/ direc
 3. Example for add new notice page.
 
 == Changelog ==
+
+= 1.4.2 =
+
+- Bug fix
 
 = 1.4.1 =
 
